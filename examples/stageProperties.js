569d74508da705063301db7d15a3a3411526a61c
Sean Lynch <tallseanlynch@gmail.com>
2021-01-19 16:14:37 -0700

    Stage sounds and colors

---
+++ examples/stageProperties.js
@@ -0,0 +1,201 @@
+const coinProperties = {
+    s: [
+        "sounds/Sound FX/Reverb Coin 1a.mp3",
+        "sounds/Sound FX/Reverb Coin 1b.mp3",
+        "sounds/Sound FX/Reverb Coin 1c.mp3"
+    ],
+    m: [
+        "sounds/Sound FX/Reverb Coin 2a.mp3",
+        "sounds/Sound FX/Reverb Coin 2b.mp3",
+        "sounds/Sound FX/Reverb Coin 2c.mp3"
+    ],
+    l: [
+        "sounds/Sound FX/Reverb Coin 3a.mp3",
+        "sounds/Sound FX/Reverb Coin 3b.mp3",
+        "sounds/Sound FX/Reverb Coin 3c.mp3"
+    ]
+},
+
+
+const stageProperties = {
+    STAGE0: {
+        backgroundColors: [
+            "rgba(2,0,36,0)", 
+            "rgba(121,9,117,1)", 
+            "rgba(252,231,183,1)", 
+            "rgba(0,212,255,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx a1.mp3",
+                "sounds/Sound FX/BALL FALL fx a2.mp3",
+                "sounds/Sound FX/BALL FALL fx a3.mp3",
+                "sounds/Sound FX/BALL FALL fx a4.mp3",
+                "sounds/Sound FX/BALL FALL fx a5.mp3",
+                "sounds/Sound FX/BALL FALL fx a6.mp3",
+                "sounds/Sound FX/BALL FALL fx a7.mp3",
+                "sounds/Sound FX/BALL FALL fx a8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 1.mp3"
+        }
+    },
+    STAGE1: {
+        backgroundColors: [
+            "rgba(2,0,36,0)", 
+            "rgba(121,9,117,1)", 
+            "rgba(252,231,183,1)", 
+            "rgba(0,212,255,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx b1.mp3",
+                "sounds/Sound FX/BALL FALL fx b2.mp3",
+                "sounds/Sound FX/BALL FALL fx b3.mp3",
+                "sounds/Sound FX/BALL FALL fx b4.mp3",
+                "sounds/Sound FX/BALL FALL fx b5.mp3",
+                "sounds/Sound FX/BALL FALL fx b6.mp3",
+                "sounds/Sound FX/BALL FALL fx b7.mp3",
+                "sounds/Sound FX/BALL FALL fx b8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 2.mp3"
+        }
+    },
+    STAGE2: {
+        backgroundColors: [
+            "rgba(2,0,36,0)", 
+            "rgba(121,9,117,1)", 
+            "rgba(252,231,183,1)", 
+            "rgba(0,212,255,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx c1.mp3",
+                "sounds/Sound FX/BALL FALL fx c2.mp3",
+                "sounds/Sound FX/BALL FALL fx c3.mp3",
+                "sounds/Sound FX/BALL FALL fx c4.mp3",
+                "sounds/Sound FX/BALL FALL fx c5.mp3",
+                "sounds/Sound FX/BALL FALL fx c6.mp3",
+                "sounds/Sound FX/BALL FALL fx c7.mp3",
+                "sounds/Sound FX/BALL FALL fx c8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 3.mp3"
+        }
+    },
+    STAGE3: {
+        backgroundColors: [
+            "rgba(2,0,36,0)", 
+            "rgba(121,9,117,1)", 
+            "rgba(252,231,183,1)", 
+            "rgba(0,212,255,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx d1.mp3",
+                "sounds/Sound FX/BALL FALL fx d2.mp3",
+                "sounds/Sound FX/BALL FALL fx d3.mp3",
+                "sounds/Sound FX/BALL FALL fx d4.mp3",
+                "sounds/Sound FX/BALL FALL fx d5.mp3",
+                "sounds/Sound FX/BALL FALL fx d6.mp3",
+                "sounds/Sound FX/BALL FALL fx d7.mp3",
+                "sounds/Sound FX/BALL FALL fx d8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 4.mp3"
+        }
+    },
+    STAGE4: {
+        backgroundColors: [
+            "rgba(2,0,36,0)", 
+            "rgba(121,9,117,1)", 
+            "rgba(252,231,183,1)", 
+            "rgba(0,212,255,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx e1.mp3",
+                "sounds/Sound FX/BALL FALL fx e2.mp3",
+                "sounds/Sound FX/BALL FALL fx e3.mp3",
+                "sounds/Sound FX/BALL FALL fx e4.mp3",
+                "sounds/Sound FX/BALL FALL fx e5.mp3",
+                "sounds/Sound FX/BALL FALL fx e6.mp3",
+                "sounds/Sound FX/BALL FALL fx e7.mp3",
+                "sounds/Sound FX/BALL FALL fx e8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 5.mp3"
+        }
+    },
+    STAGE5: {
+        backgroundColors: [
+            "rgba(2,0,36,0)",
+            "rgba(121,9,117,1)",
+            "#2e3192",
+            "rgba(0,0,0,1)"
+        ],
+        basicColors: [
+            new THREE.Color(0xfff1e3),
+            new THREE.Color(0xffd5c9),
+            new THREE.Color(0xffa6bb),
+            new THREE.Color(0xff757f),
+            new THREE.Color(0x159294)
+        ],
+        sounds: {
+            coinProperties,
+            beams: [
+                "sounds/Sound FX/BALL FALL fx a1.mp3",
+                "sounds/Sound FX/BALL FALL fx a2.mp3",
+                "sounds/Sound FX/BALL FALL fx a3.mp3",
+                "sounds/Sound FX/BALL FALL fx a4.mp3",
+                "sounds/Sound FX/BALL FALL fx a5.mp3",
+                "sounds/Sound FX/BALL FALL fx a6.mp3",
+                "sounds/Sound FX/BALL FALL fx a7.mp3",
+                "sounds/Sound FX/BALL FALL fx a8.mp3"
+            ],
+            prize: ["sounds/Sound FX/Test Coin.mp3"],
+            backgroundMusic: "sounds/BALL FALL jingle flat 1.mp3"
+        }
+    },
+}
