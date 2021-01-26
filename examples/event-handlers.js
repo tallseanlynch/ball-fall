9c6974e9b0bbf9e598c65fe7ce3b6cbc0933514d
Sean Lynch <tallseanlynch@gmail.com>
2021-01-26 15:42:23 -0700

    Howler stopping audio on blur from window and determining the click / touch handlers

--- examples/event-handlers.js
+++ examples/event-handlers.js
@@ -1,26 +1,45 @@
 setTimeout(() => {
     document.addEventListener("touchstart", (e) => {
-        if (!window.appConfig.userHasEnded) {
-          window.appConfig.touchEnd = false;
-          window.appConfig.touchStart = true;
-          window.appConfig.currentX = e.touches[0].clientX;
-          window.appConfig.currentY = e.touches[0].clientY;
-    
-          window.appConfig.prevX = e.touches[0].clientX;
-          window.appConfig.prevY = e.touches[0].clientY;
-          window.appConfig.diffX =
-            window.appConfig.currentX - window.appConfig.prevX;
-          window.appConfig.diffY =
-            window.appConfig.currentY - window.appConfig.prevY;
-        }
-      });
-    
-      window.appConfig.events = { touchmove: { throttle: 50, active: false } }
+      if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'touch') && !window.appConfig.userHasEnded) {
+        window.appConfig.touchEnd = false;
+        window.appConfig.touchStart = true;
+        window.appConfig.currentX = e.touches[0].clientX;
+        window.appConfig.currentY = e.touches[0].clientY;
+  
+        window.appConfig.prevX = e.touches[0].clientX;
+        window.appConfig.prevY = e.touches[0].clientY;
+        window.appConfig.diffX =
+          window.appConfig.currentX - window.appConfig.prevX;
+        window.appConfig.diffY =
+          window.appConfig.currentY - window.appConfig.prevY;
+      }
+    });
+
+    document.addEventListener("mousedown", (e) => {
+      if(window.appConfig.events.primaryInput === undefined) {
+        window.appConfig.events.primaryInput = 'mouse'
+      }
+      console.log('MOUSEDOWN',e)
+      if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'mouse') && !window.appConfig.userHasEnded) {
+        window.appConfig.touchEnd = false;
+        window.appConfig.touchStart = true;
+        window.appConfig.currentX = e.clientX;
+        window.appConfig.currentY = e.clientY;
+  
+        window.appConfig.prevX = e.clientX;
+        window.appConfig.prevY = e.clientY;
+        window.appConfig.diffX =
+          window.appConfig.currentX - window.appConfig.prevX;
+        window.appConfig.diffY =
+          window.appConfig.currentY - window.appConfig.prevY;
+      }
+    });
     
       document.body.addEventListener(
         "touchmove",
         (e) => {
-          if (!window.appConfig.userHasEnded && window.appConfig.events.touchmove.active === false) {
+          window.appConfig.events.primaryInput = 'touch'
+          if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'touch') && !window.appConfig.userHasEnded && window.appConfig.events.touchmove.active === false) {
             window.appConfig.events.touchmove.active = true
             window.appConfig.prevX = window.appConfig.currentX;
             window.appConfig.prevY = window.appConfig.currentY;
@@ -38,9 +57,34 @@ setTimeout(() => {
         },
         true
       );
-    
-      document.addEventListener("touchend", (e) => {
-        if (!window.appConfig.userHasEnded) {
+
+      document.body.addEventListener(
+        "mousemove",
+        (e) => {
+          console.log('MOUSEMOVE', e)
+          if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'mouse') && !window.appConfig.userHasEnded && window.appConfig.events.touchmove.active === false) {
+            window.appConfig.events.touchmove.active = true
+            window.appConfig.prevX = window.appConfig.currentX;
+            window.appConfig.prevY = window.appConfig.currentY;
+            window.appConfig.currentX = e.clientX;
+            window.appConfig.currentY = e.clientY;
+            window.appConfig.diffX =
+              e.clientX - window.appConfig.prevX;
+            window.appConfig.diffY =
+              e.clientY - window.appConfig.prevY;
+            e.preventDefault();
+            setTimeout(() => {
+              window.appConfig.events.touchmove.active = false
+            }, window.appConfig.events.touchmove.throttle)
+          }
+        },
+        true
+      );
+
+
+      document.addEventListener("mouseup", (e) => {
+        if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'mouse') && !window.appConfig.userHasEnded) {
+          console.log('MOUSEUP', e)
           window.appConfig.touchEnd = true;
           window.appConfig.touchStart = false;
           window.appConfig.currentX = 0;
@@ -48,6 +92,33 @@ setTimeout(() => {
           window.appConfig.diffX = 0;
           window.appConfig.diffY = 0;
         }
+      });
+
+      document.addEventListener("touchend", (e) => {
+        if ((window.appConfig.events.primaryInput === undefined || window.appConfig.events.primaryInput === 'touch') && !window.appConfig.userHasEnded) {
+          window.appConfig.touchEnd = true;
+          window.appConfig.touchStart = false;
+          window.appConfig.currentX = 0;
+          window.appConfig.currentY = 0;
+          window.appConfig.diffX = 0;
+          window.appConfig.diffY = 0;
+        }
+      });
+
+      window.addEventListener('blur', function(e) {
+        e.preventDefault();
+        Howler.stop()
+        window.appConfig.gameModeFunctions.pause()
+      })
+
+      window.addEventListener('beforeunload', function(e) {
+          //following two lines will cause the browser to ask the user if they
+          //want to leave. The text of this dialog is controlled by the browser.
+          e.preventDefault(); //per the standard
+          Howler.stop()
+          window.appConfig.gameModeFunctions.pause()
+          e.returnValue = ''; //required for Chrome
+        //else: user is allowed to leave without a warning dialog
       });
 
       // const clickRay = new THREE.Raycaster();
