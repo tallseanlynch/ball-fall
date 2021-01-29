e9336537d133284a5414a82ae2efb82f9e4cae8c
Sean Lynch <tallseanlynch@gmail.com>
2021-01-28 20:59:27 -0700

    Only asks if you wanna leave the page if the user is touch or mouse moving.

--- examples/event-handlers.js
+++ examples/event-handlers.js
@@ -113,10 +113,12 @@ setTimeout(() => {
       window.addEventListener('beforeunload', function(e) {
           //following two lines will cause the browser to ask the user if they
           //want to leave. The text of this dialog is controlled by the browser.
-          e.preventDefault(); //per the standard
-          Howler.stop()
-          if(window.appConfig.start === true && window.appConfig.userHasEnded === false && window.appConfig.pause === false) {
-            window.appConfig.gameModeFunctions.pause()
+          if(window.appConfig.events.touchmove.active === true) {
+            e.preventDefault(); //per the standard
+            Howler.stop()
+            if(window.appConfig.start === true && window.appConfig.userHasEnded === false && window.appConfig.pause === false) {
+              window.appConfig.gameModeFunctions.pause()
+            }  
           }
           e.returnValue = ''; //required for Chrome
         //else: user is allowed to leave without a warning dialog
